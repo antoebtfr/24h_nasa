09954d5d2d85a5f305c0a6d5999e5d3b50a5f8c3
cdhbze93 <antoebt@hotmail.com>
2019-12-20 07:58:01 +0100

    feat: bdd modal

--- src/app/pages/bdd-pages/bdd-pages.component.ts
+++ src/app/pages/bdd-pages/bdd-pages.component.ts
@@ -1,3 +1,5 @@
+import { VariablesGlobales } from './../../variableGlobales';
+import { PlaneteService } from './../../shared/planete.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bdd-pages.component.css']
 })
 export class BddPagesComponent implements OnInit {
-
-  constructor() { }
+  clicked = false;
+  solarSys = [];
+  constructor(private _plaServ: PlaneteService, public varGlo: VariablesGlobales) { }
 
   ngOnInit() {
+    this._plaServ.getPlanets().subscribe(data => this.solarSys = data);
   }
 
+  clickElement(name, img, bio) {
+    this.clicked = !this.clicked;
+    this.varGlo.name = name;
+    this.varGlo.img = img;
+    this.varGlo.bio = bio;
+  }
 }
